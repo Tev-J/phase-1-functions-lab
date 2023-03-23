ef506b94ea7e0d6ea0b364a414c1c2db7db12945
Tev-J <tj_delph@yahoo.com>
2023-03-23 00:04:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+  let fare = 0;
+
+  if (distanceInFeet <= 400) {
+    fare = 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    fare = (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    fare = 25;
+  } else {
+    fare = "cannot travel that far";
+  }
+
+  return fare;
+}
